001626fd298342945102ed0770919547837dc123
Edu <edu.d.rai@gmail.com>
2018-10-13 11:42:14 -0300

    end of module 3

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent,args,ctx,info){
+    //TODO check if they are loged in
+    //the context is exported in createServer
+    const item = await ctx.db.mutation.createItem({
+      //title: args.title,
+      //description: args.description, etc sooo
+      data : {...args}
+    },info);
+    
+  console.log(item);
+  return item;
+}
+};
 
-module.exports = mutations;
+module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding'); //when you don't need custom logic or authentication you just fordward the queryes
+
+const Query = {
+ /*  async items(parent,args,ctx,info){
+    const items = await ctx.db.query.items();
+    return items;
+  }  */
+
+  items: forwardTo('db'), 
+};
 
 module.exports = Query;
