3c79fac40e1fd3a3374129f0062f4695934fc599
Edu <edu.d.rai@gmail.com>
2018-11-03 14:49:35 -0300

    search query on demand, showing items of the search

---
+++ sick-fits/frontend/components/Search.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import Downshift from "downshift";
+import Router from "next/router";
+import { ApolloConsumer } from "react-apollo";
+import gql from "graphql-tag";
+import debounce from "lodash.debounce";
+import { DropDown, DropDownItem, SearchStyles } from "./styles/DropDown";
+
+const SEARCH_ITEMS_QUERY = gql`
+  query SEARCH_ITEMS_QUERY($searchTerm: String!) {
+    items(
+      where: {
+        OR: [
+          { title_contains: $searchTerm }
+          { description_contains: $searchTerm }
+        ]
+      }
+    ) {
+      id
+      image
+      title
+    }
+  }
+`;
+
+class AutoComplete extends Component {
+  state = {
+    items: [],
+    loading: false
+  };
+
+  onChange = debounce(async (e, client) => {
+    console.log("searching");
+    //turn loading on
+    this.setState({
+      loading: true
+    });
+    //manually query apollo client
+    const res = await client.query({
+      query: SEARCH_ITEMS_QUERY,
+      variables: { searchTerm: e.target.value }
+    });
+    this.setState({
+      loading: false,
+      items: res.data.items
+    });
+  }, 350);
+  render() {
+    return (
+      <SearchStyles>
+        <div>
+          <ApolloConsumer>
+            {client => (
+              <input
+                type="search"
+                onChange={e => {
+                  e.persist();
+                  this.onChange(e, client);
+                }}
+              />
+            )}
+          </ApolloConsumer>
+          <DropDown>
+            {this.state.items.map(item => (
+              <DropDownItem item={item.id}>
+                <img width="50" src={item.image} alt={item.title} />
+                {item.title}
+              </DropDownItem>
+            ))}
+          </DropDown>
+        </div>
+      </SearchStyles>
+    );
+  }
+}
+
+export default AutoComplete;
