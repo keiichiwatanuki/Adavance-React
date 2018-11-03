9ebad806c2b66117823934771d550cea69280612
Edu <edu.d.rai@gmail.com>
2018-11-03 01:16:38 -0300

    animating cart count and "dealing" with removed items

--- sick-fits/frontend/components/CartItem.js
+++ sick-fits/frontend/components/CartItem.js
@@ -20,28 +20,37 @@ const CartItemStyles = styled.li`
   }
 `;
 
-const CartItem = ({ cartItem }) => (
-  <CartItemStyles>
-    <img
-      src={cartItem.item.image}
-      width="auto"
-      height="100px"
-      alt={cartItem.item.title}
-    />
-    <div className="cart-item-details">
-      <h3>{cartItem.item.title}</h3>
-      <p>
-        {formatMoney(cartItem.item.price * cartItem.quantity)}
-        {" - "}{" "}
-        <em>
-          {cartItem.quantity} &times; {formatMoney(cartItem.item.price)}
-          {" each"}
-        </em>
-      </p>
-    </div>
-    <RemoveFromCart id={cartItem.id} />
-  </CartItemStyles>
-);
+const CartItem = ({ cartItem }) => {
+  //first check if that item exists
+  if (!cartItem.item)
+    return (
+      <CartItemStyles>
+        <p>this item has been removed</p> <RemoveFromCart id={cartItem.id} />
+      </CartItemStyles>
+    );
+  return (
+    <CartItemStyles>
+      <img
+        src={cartItem.item.image}
+        width="auto"
+        height="100px"
+        alt={cartItem.item.title}
+      />
+      <div className="cart-item-details">
+        <h3>{cartItem.item.title}</h3>
+        <p>
+          {formatMoney(cartItem.item.price * cartItem.quantity)}
+          {" - "}{" "}
+          <em>
+            {cartItem.quantity} &times; {formatMoney(cartItem.item.price)}
+            {" each"}
+          </em>
+        </p>
+      </div>
+      <RemoveFromCart id={cartItem.id} />
+    </CartItemStyles>
+  );
+};
 
 //make the proptype for cartItem
 export default CartItem;
